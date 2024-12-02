7bf4018daf15ac717f947a1cec5ca6f5181f7500
Matsunaru <error1998pl@gmail.com>
2024-12-02 18:54:06 +0100

    finish

--- Sorting_algorithm_visualized_in_the_console.cpp
+++ Sorting_algorithm_visualized_in_the_console.cpp
@@ -2,12 +2,20 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+#include <thread>
+
+//func that cheking if user input is number or not
 int Only_Number();
+//generete numbers from 0 to 30, from user input
 void Generte_numbers(int number,int* array);
-void Quick_Sort(int* Array, int low, int high);
-void Display_Sort();
+//Quick Sort algorytm
+void Quick_Sort(int* Array, int low, int high, int number1);
+//visualization of sorting process
+void Display_Sort(int* Array,int number);
+//helping void that make easier to change numbers
 void swap(int& a, int& b);
-int partition(int* Array, int low, int high);
+//Key element of Quick sort algo
+int partition(int* Array, int low, int high,int number1);
 
 
 
@@ -18,11 +26,9 @@ int main()
     int number=Only_Number();
     int* Array = new int[number];//dynamic Array
     Generte_numbers(number,Array);
-    Quick_Sort(Array, 0, number-1);
-    for (int i = 0; i < number; i++)
-    {
-        std::cout << Array[i];
-    }
+    Display_Sort(Array, number);// Display the array before sorting
+    Quick_Sort(Array, 0, number-1,number);// Perform Quick Sort
+    Display_Sort(Array, number);// Display the sorted array
 }
 int Only_Number()
 {
@@ -33,56 +39,79 @@ int Only_Number()
         {
             return input;
         }
-        else//if not give him another chance
+        else//If not, prompt the user to try again
         {
             std::cout << "We accept only number Bro: ";
-            std::cin.clear();
+            std::cin.clear();// Clear the error flag
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
 }
 void Generte_numbers(int number,int* Array)
 {
-    //int* Array = new int[number];//dynamic Array
-    srand(time(NULL));
+    srand(time(NULL));//Initialize the random number generator
     for (int i = 0; i < number; i++)//gennerating numbers from 1 to 30
     {
        Array[i] = rand() % 30 + 1;
     }
-    for (int i = 0; i < number; i++)
-    {
-        std::cout << Array[i] << std::endl;
-    }
 }
-void Quick_Sort(int* Array,int low,int high)
+void Quick_Sort(int* Array,int low,int high,int number1)
 {
     if (low < high)
     {
-        int PivotIndex = partition(Array, low, high);
-        Quick_Sort(Array, low, PivotIndex - 1);
-        Quick_Sort(Array, PivotIndex + 1,high);
+        //Pivot returning Array in 2 parts and return the pivot index
+        int PivotIndex = partition(Array, low, high,number1);
+        // Recursively sort the left part
+        Quick_Sort(Array, low, PivotIndex - 1,number1);
+        // Recursively sort the right part
+        Quick_Sort(Array, PivotIndex + 1,high,number1);
 
     }
 }
 void swap(int& a, int& b)
 {
+    // Swap the values of two variables
     int temp = a;
     a = b;
     b = temp;
 }
-int partition(int* Array, int low, int high)
+int partition(int* Array, int low, int high,int number1)
 {
-    int pivot = Array[high];
+    int pivot = Array[high]; // Set the last element as the pivot
     int i = low - 1;
-
+    //loop that loop from begining of Array to end
     for (int j = low; j < high; j++)
     {
-        if (Array[j] < pivot)
+        if (Array[j] < pivot)//if Array[j] is lower then pivot then execiut
         {
             i++;
-            swap(Array[i], Array[j]);
+            swap(Array[i], Array[j]);//swap element
+            std::this_thread::sleep_for(std::chrono::milliseconds(200)); // Add a delay for visualization
+            Display_Sort(Array, number1);
         }
     }
+    // Place the pivot in the correct position
     swap(Array[i + 1], Array[high]);
-    return i + 1;
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));// Add a delay for visualization
+    Display_Sort(Array, number1);
+    return i + 1;// Return the pivot index
+}
+void Display_Sort(int* Array,int number)
+{
+    system("CLS");
+    for (int p = 30; p > 0;p--)//looping 30 times 
+    {
+        for (int i = 0; i < number; i++)//looping throu how many Array there is
+        {
+            if (Array[i] < p)//if Array number is lower then p printing " "
+            {
+                std::cout << " ";
+            }
+            else//else prining "#"
+            {
+                std::cout << "#";
+            }
+        }
+        std::cout << std::endl;//Move to the next line
+    }
 }
